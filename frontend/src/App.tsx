efbf9795960e26554fe7d08f9699785f21fa3d3e
khushal <khushalsatani009@gmail.com>
2025-12-19 14:59:28 +0530

    React Project Setup with Tailwind CSS, Router & Types

---
+++ frontend/src/App.tsx
@@ -0,0 +1,29 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Pricing from "./pages/Pricing"
+import Projects from "./pages/Projects"
+import MyProjects from "./pages/MyProjects"
+import Preview from "./pages/Preview"
+import View from "./pages/View"
+import Community from "./pages/Community"
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+       
+        <Route path="/" element={<Home />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/projects/:projectId" element={<Projects />} />
+        <Route path="/projects" element={<MyProjects />} />
+        <Route path="/preview/:projectId" element={<Preview/>} />
+        <Route path="/preview/:projectId/:versionId" element={<Preview />} />
+        <Route path="/comminity" element={<Community />} />
+        <Route path="/view/:projectId" element={<View/>} />
+        
+      </Routes>
+    </div>
+  )
+}
+
+export default App
